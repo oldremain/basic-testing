aad890927fcc885d4e2d681c267d105f11684375
Олег Зайцев <30oleg.zv94@gmail.com>
2024-02-12 18:38:08 +0300

    feat: implement simple-tests, table-tests

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,17 +1,73 @@
 // Uncomment the code below and write your tests
-/* import {  simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
+
+enum Matcher {
+  'ToEqual' = 'toEqual',
+  'ToBeNull' = 'toBeNull',
+}
 
 const testCases = [
-    { a: 1, b: 2, action: Action.Add, expected: 3 },
-    { a: 2, b: 2, action: Action.Add, expected: 4 },
-    { a: 3, b: 2, action: Action.Add, expected: 5 },
-    // continue cases for other actions    
-]; */
+  {
+    a: 1,
+    b: 2,
+    action: Action.Add,
+    expected: 3,
+    assert: Matcher.ToEqual,
+  },
+  {
+    a: 4,
+    b: 2,
+    action: Action.Subtract,
+    expected: 2,
+    assert: Matcher.ToEqual,
+  },
+  {
+    a: 3,
+    b: 2,
+    action: Action.Multiply,
+    expected: 6,
+    assert: Matcher.ToEqual,
+  },
+  {
+    a: 10,
+    b: 2,
+    action: Action.Divide,
+    expected: 5,
+    assert: Matcher.ToEqual,
+  },
+  {
+    a: 10,
+    b: 2,
+    action: Action.Exponentiate,
+    expected: 100,
+    assert: Matcher.ToEqual,
+  },
+  { a: 4, b: 2, action: 'x', expected: null, assert: Matcher.ToBeNull },
+  {
+    a: 4,
+    b: '2',
+    action: Action.Add,
+    expected: null,
+    assert: Matcher.ToBeNull,
+  },
+];
 
-describe('simpleCalculator', () => {
-  // This test case is just to run this test suite, remove it when you write your own tests
-  test('should blah-blah', () => {
-    expect(true).toBe(true);
-  });
-  // Consider to use Jest table tests API to test all cases above
-});
+describe.each(testCases)(
+  'simpleCalculator({a: $a, b: $b, action: $action})',
+  ({ a, b, action, expected, assert }) => {
+    switch (assert) {
+      case Matcher.ToEqual:
+        test(`should return ${expected}`, () => {
+          expect(simpleCalculator({ a, b, action })).toEqual(expected);
+        });
+        break;
+      case Matcher.ToBeNull:
+        test(`should return ${expected}`, () => {
+          expect(simpleCalculator({ a, b, action })).toBeNull();
+        });
+        break;
+      default:
+        throw new Error(`Unexpected matcher - ${assert}`);
+    }
+  },
+);
